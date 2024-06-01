32e975217e75b8c62097b862e37c46e6f3fa7b03
Ritam-Vaskar <vaskarananda77@gmail.com>
2024-06-01 13:01:21 +0530

    eventPopupImg

--- src/components/EventPopup/EventPopup.jsx
+++ src/components/EventPopup/EventPopup.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './EventPopup.module.scss';
+import eventImg from '../../data/images/EventPopup.png'
 
 const EventPopup = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -21,7 +22,8 @@ const EventPopup = () => {
     <div className={`${styles.popup} ${isVisible ? styles.fadeIn : ''}`}>
       <div className={styles.popupContent}>
         <button className={styles.closeButton} onClick={closePopup}>×</button>
-        <p>This is a popup message.</p>
+        {/* <p>This is a popup message.</p> */}
+        <a href="#"><img src={eventImg} alt="To Fed" /></a>
       </div>
     </div>
   );
