ab90952c6665d819ab6600f38aba0aaec29ef9d6
myusername <115021946+saurav1729@users.noreply.github.com>
2024-06-01 23:21:20 +0530

    signUp added && basic style changes

---
+++ src/components/SignUp/GoogleSignup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import GoogleIcon from "../../data/images/googleIcon.svg"
+import Gscss from "./style/SignupFun.module.scss";
+
+const GoogleSignup = () => {
+  return (
+    <>
+    <div className={Gscss.Gdiv}>
+      <img className={Gscss.Icon} src={GoogleIcon}></img>
+      
+    <p>Sign Up with google</p>
+      </div>
+    </>
+  )
+}
+
+export default GoogleSignup
