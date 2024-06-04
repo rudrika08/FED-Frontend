e0dda93c43ab95089d70c70ef1e7ba16abebaec1
Prityanshu Singh <84603998+PrityanshuSingh@users.noreply.github.com>
2024-06-05 01:45:07 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 import Carousel from "../../components/Carousel/styles/Carousel"
 import { CarouselImg } from "../../data/carouselImages";
 import Sponsered from "../../components/Sponser/Sponsered";
-import HeroSection from "../../components/hero/HeroSection";
+import HeroSection from "../../components/Hero/HeroSection";
 import Feedback from "../../components/Feedback/Feedback" ;
 import Contact from "../../components/Contact/Contact";
 import EventPopup from "../../components/EventPopup/EventPopup"
