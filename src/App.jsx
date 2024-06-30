79aee44f8e06b79841e7b24e3ba1e57144d1ba38
Ashmit Sinha <ashmitsinha15@gmail.com>
2024-06-30 16:28:53 +0530

    Added edit profile modal, fixed profile and chatbot css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route ,Outlet, Navigate} from "react-router-dom";
 import Navbar from "./layouts/Navbar/Navbar";
 import MobileNavbar from "./layouts/MobileNavbar/MobileNavbar";
 import Footer from "./layouts/Footer/Footer";
+import EditProfile from "./features/Modals/EditProfile/EditProfile";
 
 // pages
 const Home = lazy(() => import("./pages/Home/Home"));
@@ -83,7 +84,7 @@ function App() {
       </Routes>
     </Suspense>
    
-
+  {/* <EditProfile/> */}
      
     </div>
   );
