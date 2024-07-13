00a6d161d831e4cfb06350a1961818845aa64111
Ritam-Vaskar <vaskarananda77@gmail.com>
2024-07-14 00:12:11 +0530

    className fix

--- src/layouts/Skeleton/Carousel/Carousel.jsx
+++ src/layouts/Skeleton/Carousel/Carousel.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import classNames from "classnames";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import skeletonStyles from "./styles/Carousel.module.scss";
 
 const CarouselSkeleton = ({ customStyles = {} }) => (
   <SkeletonTheme baseColor="#525252" highlightColor="#313131">
-    <div className={classNames(skeletonStyles.carousel_skeleton, customStyles.carousel_skeleton)}>
-    <div className={classNames(skeletonStyles.inner_skeleton, customStyles.inner_skeleton)}>
-    <div className={classNames(skeletonStyles.carousel_outer, customStyles.carousel_outer)}>
-      <Skeleton className={classNames(skeletonStyles.carousel, customStyles.carousel)} />
+    <div 
+      className={`${skeletonStyles.carousel_skeleton} ${customStyles.carousel_skeleton || ''}`}
+    >
+      <div 
+        className={`${skeletonStyles.inner_skeleton} ${customStyles.inner_skeleton || ''}`}
+      >
+        <div 
+          className={`${skeletonStyles.carousel_outer} ${customStyles.carousel_outer || ''}`}
+        >
+          <Skeleton 
+            className={`${skeletonStyles.carousel} ${customStyles.carousel || ''}`} 
+          />
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-   
   </SkeletonTheme>
 );
 
