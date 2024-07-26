4f5b44c9220c19eb30ab2cb2a19fce3da832983c
Ritam-Vaskar <vaskarananda77@gmail.com>
2024-07-27 00:36:40 +0530

    css&skeleton(fixes remain)

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,31 +1,57 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 import style from "./styles/Event.module.scss";
 import { EventCard } from "../../components";
-import FormData from "../../data/FormData.json";
+// import EventData from "../../data/eventData.json";
+import FormData from "../../data/FormData.json"
 import ring from "../../assets/images/ring.svg";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import EventCardSkeleton from "../../layouts/Skeleton/EventCard/EventCardSkeleton";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 const Event = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const { events } = FormData;
-  const [eventData, setEventData] = useState(events);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        // Simulate a loading delay for demonstration purposes
+        setTimeout(() => {
+          const localEventData = events;
+          setEventData(localEventData);
+          setLoading(false);
+        }, 2000); // 2 seconds delay
+      } catch (error) {
+        console.error('Error fetching events:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
 
   const ongoingEvents = eventData.filter((event) => !event.info.isEventPast);
   const pastEvents = eventData.filter((event) => event.info.isEventPast);
 
   const customStyles = {
     eventname: {
-      paddingTop: "5px",
       fontSize: "1.2rem",
     },
     registerbtn: {
-      width: "7rem",
-      fontSize: ".721rem",
+      width: "8rem",
+      fontSize: ".721rem"
+    },
+    eventnamep: {
+      fontSize: "0.7rem",
     },
   };
 
@@ -34,48 +60,75 @@ const Event = () => {
       <div style={{ display: "flex" }}>
         <div className={style.line}></div>
         <div className={style.eventwhole}>
-          {ongoingEvents.length > 0 && (
-            <div className={style.eventcard}>
-              <div className={style.name}>
-                <img className={style.ring1} src={ring} alt="ring" />
-                <span className={style.w1}>Ongoing</span>
-                <span className={style.w2}>Events</span>
-              </div>
-              <div className={style.cardsin}>
-                {ongoingEvents.map((event, index) => (
-                  <div style={{ height: "auto", width: "22rem" }} key={index}>
-                    <EventCard
-                      data={event}
-                      onOpen={() => console.log("Event opened")}
-                      type="ongoing"
-                      customStyles={customStyles}
-                      modalpath='/Events/'
-                      aosDisable={false}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-          <div className={style.pasteventcard} style={{ marginTop: ongoingEvents.length > 0 ? "6rem" : "1rem" }}>
-            <div className={style.name}>
-              <img className={style.ring2} src={ring} alt="ring" />
-              <span className={style.w1}>Past</span>
-              <span className={style.w2}>Events</span>
-            </div>
-            <div className={style.cardone}>
-              {pastEvents.map((event, index) => (
-                <div style={{ height: "auto", width: "22rem" }} key={index}>
-                  <EventCard
-                    data={event}
-                    type="past"
-                    customStyles={customStyles}
-                    modalpath='/Events/pastEvents/'
-                  />
+          {loading ? (
+            <SkeletonTheme color="#e0e0e0" highlightColor="#f5f5f5">
+              <div className={style.eventcard}>
+                <div className={style.name}>
+                  <img className={style.ring1} src={ring} alt="ring" />
+                  <span className={style.w1}>Ongoing</span>
+                  <span className={style.w2}>Events</span>
                 </div>
-              ))}
-            </div>
-          </div>
+                <div className={style.cardsin}>
+                  <EventCardSkeleton amount={2} />
+                </div>
+              </div>
+              <div className={style.pasteventcard} style={{ marginTop: "1rem" }}>
+                <div className={style.name}>
+                  <img className={style.ring2} src={ring} alt="ring" />
+                  <span className={style.w1}>Past</span>
+                  <span className={style.w2}>Events</span>
+                </div>
+                <div className={style.cardone}>
+                  <EventCardSkeleton amount={2} />
+                </div>
+              </div>
+            </SkeletonTheme>
+          ) : (
+            <>
+              {ongoingEvents.length > 0 && (
+                <div className={style.eventcard}>
+                  <div className={style.name}>
+                    <img className={style.ring1} src={ring} alt="ring" />
+                    <span className={style.w1}>Ongoing</span>
+                    <span className={style.w2}>Events</span>
+                  </div>
+                  <div className={style.cardsin}>
+                    {ongoingEvents.map((event, index) => (
+                      <div style={{ height: "auto", width: "22rem" }} key={index}>
+                        <EventCard
+                          data={event}
+                          onOpen={() => console.log("Event opened")}
+                          type="ongoing"
+                          customStyles={customStyles}
+                          modalpath='/Events/'
+                          aosDisable={false}
+                  />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+              <div className={style.pasteventcard} style={{ marginTop: ongoingEvents.length > 0 ? "6rem" : "1rem" }}>
+                <div className={style.name}>
+                  <img className={style.ring2} src={ring} alt="ring" />
+                  <span className={style.w1}>Past</span>
+                  <span className={style.w2}>Events</span>
+                </div>
+                <div className={style.cardone}>
+                  {pastEvents.map((event, index) => (
+                    <div style={{ height: "auto", width: "22rem" }} key={index}>
+                      <EventCard
+                        data={event}
+                        type="past"
+                        customStyles={customStyles}
+                        modalpath='/Events/pastEvents/'
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
 
