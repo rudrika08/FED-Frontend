badfee646734bfbefc2a62ed5c158cc58993e1dc
Prityanshu Singh <prityanshusingh2003@gmail.com>
2024-08-04 23:37:48 +0530

    Fixed Css

--- src/features/Modals/EditProfile/EditImage.jsx
+++ src/features/Modals/EditProfile/EditImage.jsx
@@ -114,24 +114,6 @@ const EditImage = (props) => {
     }
   };
 
-  const handleUpload=()=>{
-
-    if (editorRef.current && selectedFile) {
-      setIsLoading(true);
-      const canvas = editorRef.current.getImageScaledToCanvas();
-      canvas.toBlob(async (blob) => {
-        const imageFile = new File([blob], "profile.jpg", { type: "image/jpeg" });
-      
-
-
-    setimgprv(URL.createObjectURL(blob));
-    closeModal();
-      }
-    );
-  }
-   
-  }
-
   return (
     <div style={{
       position: "fixed",
