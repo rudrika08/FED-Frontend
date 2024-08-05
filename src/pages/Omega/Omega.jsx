dd07403ae9bbe47276508b8b17fe58b69c20f320
Vinayak Puranik <vinayakpuranik0707@gmail.com>
2024-08-05 20:16:42 +0530

    Event and TeamImage Section Added

--- src/pages/Omega/Omega.jsx
+++ src/pages/Omega/Omega.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import Event from "../../sections/Omega/Event/Event.jsx";
+import TeamImage from "../../sections/Omega/TeamImage/TeamImage.jsx"
+
 
 function Omega() {
   return (
-    <div>
+    <>
       <h1 style={{margin:"8rem"}}>Currently in Development</h1>
-    </div>
+      <Event/>
+      <TeamImage/>
+    </>
   )
 }
 
