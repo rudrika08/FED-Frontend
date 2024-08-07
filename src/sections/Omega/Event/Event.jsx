bed67606aa040e2a6db3014ffb8dd32e68e329bd
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2024-08-08 03:46:40 +0530

    changes in file to secure every push

--- src/sections/Omega/Event/Event.jsx
+++ src/sections/Omega/Event/Event.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import styles from './styles/Event.module.scss';
 import data from '../../../data/omega/Event.json';
