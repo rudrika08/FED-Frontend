ab7939ded914f0b1b58f66561f21c159be2defb3
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2024-08-09 16:16:48 +0530

    Fixed back cirlces in hero and event section

--- src/pages/Omega/Omega.jsx
+++ src/pages/Omega/Omega.jsx
@@ -7,8 +7,8 @@ import Hero from '../../sections/Omega/Hero/Hero.jsx';
 import Accordion from '../../sections/Omega/Accordion/Accordion';
 import Sponsors from '../../sections/Omega/Sponsors/Sponsors';
 import Event from "../../sections/Omega/Event/Event.jsx";
-import TeamImage from "../../sections/Omega/TeamImage/TeamImage.jsx";
-import Attend from "../../sections/Omega/Attend/Attend.jsx';
+import TeamImage from "../../sections/Omega/TeamImage/TeamImage";
+import Attend from "../../sections/Omega/Attend/Attend";
 import { ChatBot } from '../../features';
 
 function Omega() {
