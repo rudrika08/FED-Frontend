799c9e31d302f3904f5b16c8cbfc92969fc1fb87
FED-Tech <128247576+fed-tech@users.noreply.github.com>
2024-12-29 00:18:16 +0530

    Update Hero.jsx

--- src/sections/LiveEvents/Gsoc/Hero/Hero.jsx
+++ src/sections/LiveEvents/Gsoc/Hero/Hero.jsx
@@ -151,7 +151,7 @@ function Hero({ ongoingEvents, isRegisteredInRelatedEvents, eventName }) {
           : remainingTime || "REGISTER NOW"
       );
 
-      setTimeout(() => setIsMicroLoading(false), 3000);
+      setTimeout(() => setIsMicroLoading(false), 2500);
     };
 
     updateButtonText();
