469ac3c3e5f871ff5fe390c32771a03dfba0ed40
yashsirola9410 <yashtechid9410@gmail.com>
2025-01-13 00:34:19 +0530

    fixes/CSS/IMG URL/LAYOUT

--- src/sections/Profile/Admin/Form/CertificateForm/CertificateForm.jsx
+++ src/sections/Profile/Admin/Form/CertificateForm/CertificateForm.jsx
@@ -1,4 +1,6 @@
+
 import React, { useState } from "react";
+import styles from "../CertificateForm/styles/CertificateForm.module.scss";
 
 const CertificateForm = () => {
   const [name, setName] = useState("Prityanshu Singh");
@@ -6,145 +8,139 @@ const CertificateForm = () => {
   const [y, setY] = useState(200); // Adjusted to ensure visibility
   const [fontSize, setFontSize] = useState(20); // Reduced for better alignment
   const [fontColor, setFontColor] = useState("black");
-
-  const imageURL =
-    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/677d7b542222b706bfd2d571_Omega%20OC%20-%20Certificate.pdf.png";
+  const [imgURL, setImgURL] = useState(
+    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/677d7b542222b706bfd2d571_Omega%20OC%20-%20Certificate.pdf.png"
+  );
 
   const handleNameChange = (e) => setName(e.target.value);
   const handleXChange = (e) => setX(Number(e.target.value));
   const handleYChange = (e) => setY(Number(e.target.value));
   const handleFontSizeChange = (e) => setFontSize(Number(e.target.value));
   const handleFontColorChange = (e) => setFontColor(e.target.value);
+  const handleImgURLChange = (e) => setImgURL(e.target.value);
 
   return (
-    <div style={{ textAlign: "center", margin: "20px" }}>
-      <h1>Certificate Form</h1>
-      <table
-        style={{
-          margin: "20px auto",
-          borderCollapse: "collapse",
-          width: "60%",
-          textAlign: "left",
-          fontSize: "16px",
-        }}
-      >
+    <div className={styles.container}>
+      <div className={styles.certificateContainer}>
+        <img
+          src={imgURL}
+          alt="Certificate"
+          className={styles.certificateImage}
+        />
+        <div
+          className={styles.text}
+          style={{
+            left: `${x}px`,
+            top: `${y}px`,
+            fontSize: `${fontSize}px`,
+            color: fontColor,
+          }}
+        >
+          {name}
+        </div>
+      </div>
+
+      <h1 className={styles.heading}>Certificate Form</h1>
+      <table className={styles.table}>
         <tbody>
           <tr>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>Name:</td>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+            <td className={styles.td}>Name:</td>
+            <td className={styles.td}>
               <input
                 type="text"
                 value={name}
                 onChange={handleNameChange}
-                style={{
-                  width: "100%",
-                  padding: "8px",
-                  fontSize: "14px",
-                  boxSizing: "border-box",
-                }}
+                className={styles.input}
               />
             </td>
           </tr>
           <tr>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-              X Position:
-            </td>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-              <input
-                type="number"
-                value={x}
-                onChange={handleXChange}
-                style={{
-                  width: "100%",
-                  padding: "8px",
-                  fontSize: "14px",
-                  boxSizing: "border-box",
-                }}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-              Y Position:
-            </td>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-              <input
-                type="number"
-                value={y}
-                onChange={handleYChange}
-                style={{
-                  width: "100%",
-                  padding: "8px",
-                  fontSize: "14px",
-                  boxSizing: "border-box",
-                }}
-              />
+            <td className={styles.td}>X Position:</td>
+            <td className={styles.td}>
+              <div className={styles.inputWithButtons}>
+                <button
+                  className={styles.arrowButton}
+                  onClick={() => setX(x - 1)}
+                >
+                  ▲
+                </button>
+                <input
+                  type="number"
+                  value={x}
+                  onChange={handleXChange}
+                  className={styles.input}
+                />
+                <button
+                  className={styles.arrowButton}
+                  onClick={() => setX(x + 1)}
+                >
+                  ▼
+                </button>
+              </div>
             </td>
           </tr>
           <tr>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-              Font Size:
+            <td className={styles.td}>Y Position:</td>
+            <td className={styles.td}>
+              <div className={styles.inputWithButtons}>
+                <button
+                  className={styles.arrowButton}
+                  onClick={() => setY(y - 1)}
+                >
+                  ▲
+                </button>
+                <input
+                  type="number"
+                  value={y}
+                  onChange={handleYChange}
+                  className={styles.input}
+                />
+                <button
+                  className={styles.arrowButton}
+                  onClick={() => setY(y + 1)}
+                >
+                  ▼
+                </button>
+              </div>
             </td>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+          </tr>
+          <tr>
+            <td className={styles.td}>Font Size:</td>
+            <td className={styles.td}>
               <input
                 type="number"
                 value={fontSize}
                 onChange={handleFontSizeChange}
-                style={{
-                  width: "100%",
-                  padding: "8px",
-                  fontSize: "14px",
-                  boxSizing: "border-box",
-                }}
+                className={styles.input}
               />
             </td>
           </tr>
           <tr>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
-              Font Color:
-            </td>
-            <td style={{ padding: "10px", border: "1px solid #ddd" }}>
+            <td className={styles.td}>Font Color:</td>
+            <td className={styles.td}>
               <input
                 type="text"
                 value={fontColor}
                 onChange={handleFontColorChange}
-                style={{
-                  width: "100%",
-                  padding: "8px",
-                  fontSize: "14px",
-                  boxSizing: "border-box",
-                }}
+                className={styles.input}
               />
             </td>
           </tr>
         </tbody>
       </table>
-      <div
-        style={{
-          position: "relative",
-          display: "inline-block",
-          marginTop: "20px",
-        }}
-      >
-        <img
-          src={imageURL}
-          alt="Certificate"
-          style={{ width: "800px", height: "auto" }}
-        />
-        <div
-          style={{
-            position: "absolute",
-            left: `${x}px`,
-            top: `${y}px`,
-            fontSize: `${fontSize}px`,
-            color: fontColor,
-            transform: "translate(-50%, -50%)",
-            whiteSpace: "nowrap",
-            zIndex: 10,
-            fontWeight: "bold", // Optional: to make the text stand out
-          }}
-        >
-          {name}
+
+      <div className={styles.fixedBottom}>
+        <div className={styles.imgURLInputContainer}>
+          <label htmlFor="imgURL" className={styles.label}>
+            Image URL:
+          </label>
+          <input
+            type="text"
+            id="imgURL"
+            value={imgURL}
+            onChange={handleImgURLChange}
+            className={styles.input}
+          />
         </div>
       </div>
     </div>
